6eb4d003eb2d04d43820f98da510f0d80f10cb40
IFAMac <iliyasifa@gmail.com>
2022-06-29 10:17:23 +0530

    commit on 'master'

--- lib/main.dart
+++ lib/main.dart
@@ -30,58 +30,98 @@ class _HomeWidgetState extends State<HomeWidget> {
   bool _bottomExpanded = false;
 
   toggleTop() {
-    setState(() => _topExpanded = !_topExpanded);
+    setState(
+      () => _topExpanded = !_topExpanded,
+    );
   }
 
   toggleBottom() {
-    setState(() => _bottomExpanded = !_bottomExpanded);
+    setState(
+      () => _bottomExpanded = !_bottomExpanded,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     Container topContainer = Container(
       decoration: BoxDecoration(
-          border: Border.all(color: Colors.black, width: 1.0),
-          color: Colors.green),
+        border: Border.all(
+          color: Colors.black,
+          width: 1.0,
+        ),
+        color: Colors.green,
+      ),
       padding: const EdgeInsets.all(10.0),
       child: const Text('Top Container'),
     );
     Container bottomContainer = Container(
       decoration: BoxDecoration(
-          border: Border.all(color: Colors.black, width: 1.0),
-          color: Colors.amber),
+        border: Border.all(
+          color: Colors.black,
+          width: 1.0,
+        ),
+        color: Colors.amber,
+      ),
       padding: const EdgeInsets.all(10.0),
       child: const Text('Bottom Container'),
     );
-    Widget topWidget =
-        _topExpanded ? Expanded(child: topContainer) : topContainer;
-    Widget bottomWidget =
-        _bottomExpanded ? Expanded(child: bottomContainer) : bottomContainer;
+    Widget topWidget = _topExpanded
+        ? Expanded(
+            child: topContainer,
+          )
+        : topContainer;
+    Widget bottomWidget = _bottomExpanded
+        ? Expanded(
+            child: bottomContainer,
+          )
+        : bottomContainer;
     return Scaffold(
-        appBar: AppBar(title: const Text("Expanded"), actions: [
+      appBar: AppBar(
+        title: const Text("Expanded"),
+        actions: [
           TextButton.icon(
-              style: TextButton.styleFrom(backgroundColor: Colors.cyan),
-              icon: Icon(_topExpanded ? Icons.expand_more : Icons.expand_less,
-                  color: Colors.white),
-              label: const Text(
-                "Top",
-                style: TextStyle(color: Colors.white),
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.cyan,
+            ),
+            icon: Icon(
+              _topExpanded ? Icons.expand_more : Icons.expand_less,
+              color: Colors.white,
+            ),
+            label: const Text(
+              "Top",
+              style: TextStyle(
+                color: Colors.white,
               ),
-              onPressed: () => toggleTop()),
-          TextButton.icon(
-              style: TextButton.styleFrom(backgroundColor: Colors.cyan),
-              icon: Icon(
-                  _bottomExpanded ? Icons.expand_less : Icons.expand_more,
-                  color: Colors.white),
-              label:
-                  const Text("Bottom", style: TextStyle(color: Colors.white)),
-              onPressed: () => toggleBottom())
-        ]),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[topWidget, bottomWidget],
+            ),
+            onPressed: () => toggleTop(),
           ),
-        ));
+          TextButton.icon(
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.cyan,
+            ),
+            icon: Icon(
+              _bottomExpanded ? Icons.expand_less : Icons.expand_more,
+              color: Colors.white,
+            ),
+            label: const Text(
+              "Bottom",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () => toggleBottom(),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            topWidget,
+            bottomWidget,
+          ],
+        ),
+      ),
+    );
   }
 }
